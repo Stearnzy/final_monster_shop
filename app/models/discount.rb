c75077d5d2d08fbdc85b260b168e500d7ca60ec9
Zach Stearns <zach.stearns92@gmail.com>
2020-11-06 22:00:28 -0700

    Add optional discount id to item orders

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,6 @@
 class Discount < ApplicationRecord
+  validates_presence_of :quantity, :discount
+
   belongs_to :merchant
+  has_many :item_orders
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -3,5 +3,11 @@ require 'rails_helper'
 RSpec.describe Discount, type: :model do
   describe "relationships" do
     it { should belong_to :merchant }
+    it { should have_many :item_orders }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :quantity}
+    it { should validate_presence_of :discount}
   end
 end
