c87d54d93a4be13fca6f5fdd5a0fe7af10dfb600
Zach Stearns <zach.stearns92@gmail.com>
2020-11-06 22:44:35 -0700

    Rewrite test

--- spec/features/merchant/discounts/index_spec.rb
+++ spec/features/merchant/discounts/index_spec.rb
@@ -2,8 +2,27 @@ require 'rails_helper'
 
 describe "As a merchant employee" do
   describe "when I visit the discounts index page" do
+    before(:each) do
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 23137)
+
+      @merch_employee = User.create!({
+        name: "Kyle",
+        address: "333 Starlight Ave.",
+        city: "Bakersville",
+        state: "NV",
+        zip: '90210',
+        email: "kyle@email.com",
+        password: "word",
+        role: 1,
+        merchant_id: @bike_shop.id
+        })
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merch_employee)
+    end
+
     it "I see a link to create a discount" do
       visit '/merchant/discounts'
+      expect(page).to have_content('All Discounts')
       expect(page).to have_link('Create New Discount')
     end
 
@@ -13,6 +32,7 @@ describe "As a merchant employee" do
       click_link('Create New Discount')
 
       expect(current_path).to eq('/merchant/discounts/new')
+      expect(page).to have_content('Create Discount')
       expect(page).to have_field(:quantity)
       expect(page).to have_field(:discount)
     end
