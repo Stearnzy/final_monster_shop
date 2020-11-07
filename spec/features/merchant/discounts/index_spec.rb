2ff69127d5b4f0e04fddcc95fad1e028acebedd8
Zach Stearns <zach.stearns92@gmail.com>
2020-11-06 22:49:35 -0700

    Change test

--- spec/features/merchant/discounts/index_spec.rb
+++ spec/features/merchant/discounts/index_spec.rb
@@ -26,15 +26,10 @@ describe "As a merchant employee" do
       expect(page).to have_link('Create New Discount')
     end
 
-    it "When I click the create link I am taken to a form page where I see a form
-        to enter the discount's required quantity and percentage to discount by" do
+    it "When I click the create link I am taken to the discount create page" do
       visit '/merchant/discounts'
       click_link('Create New Discount')
-
       expect(current_path).to eq('/merchant/discounts/new')
-      expect(page).to have_content('Create Discount')
-      expect(page).to have_field(:quantity)
-      expect(page).to have_field(:discount)
     end
   end
 end
