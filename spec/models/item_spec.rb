441d94681362539c95a122ae132b33754c1d1674
Zach Stearns <zach.stearns92@gmail.com>
2020-11-08 19:54:39 -0700

    Add applicable_discount test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -65,23 +65,23 @@ describe Item, type: :model do
     end
   end
 
-  describe 'class methods' do
+  describe 'stat methods' do
     before(:each) do
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+      @bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
 
-      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 112)
-      @handlebars = @meg.items.create(name: "Handle Bars", description: "Grip it", price: 50, image: "https://cdn.shopify.com/s/files/1/2191/9809/products/bullhorn2__95733.1479507031.1280.960_1024x1024.jpg?v=1501527872", inventory: 243)
-      @chain = @meg.items.create(name: "Chain", description: "Strong", price: 25, image: "https://dks.scene7.com/is/image/GolfGalaxy/19SCWUBMX12X18CHRTAM?qlt=70&wid=600&fmt=pjpeg", inventory: 232)
-      @pedal = @meg.items.create(name: "Pedal", description: "Step on it", price: 10, image: "https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/11/2020/06/meow.jpg", inventory: 374)
-      @reflector = @meg.items.create(name: "Reflector", description: "They'll see you", price: 200, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcStDVPL-iMrlZHu5mai-EXeyDH-wH2r9nBeFw&usqp=CAU", inventory: 324)
-      @seat = @meg.items.create(name: "Seat", description: "Soft", price: 123, image: "https://i5.walmartimages.com/asr/597375a9-fb8f-4a47-a4a2-28201888754a_1.3ebe254d3fea4ff52d5578eda9e44f77.jpeg", inventory: 217)
+      @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 112)
+      @handlebars = @bike_shop.items.create(name: "Handle Bars", description: "Grip it", price: 50, image: "https://cdn.shopify.com/s/files/1/2191/9809/products/bullhorn2__95733.1479507031.1280.960_1024x1024.jpg?v=1501527872", inventory: 243)
+      @chain = @bike_shop.items.create(name: "Chain", description: "Strong", price: 25, image: "https://dks.scene7.com/is/image/GolfGalaxy/19SCWUBMX12X18CHRTAM?qlt=70&wid=600&fmt=pjpeg", inventory: 232)
+      @pedal = @bike_shop.items.create(name: "Pedal", description: "Step on it", price: 10, image: "https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/11/2020/06/meow.jpg", inventory: 374)
+      @reflector = @bike_shop.items.create(name: "Reflector", description: "They'll see you", price: 200, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcStDVPL-iMrlZHu5mai-EXeyDH-wH2r9nBeFw&usqp=CAU", inventory: 324)
+      @seat = @bike_shop.items.create(name: "Seat", description: "Soft", price: 123, image: "https://i5.walmartimages.com/asr/597375a9-fb8f-4a47-a4a2-28201888754a_1.3ebe254d3fea4ff52d5578eda9e44f77.jpeg", inventory: 217)
 
-      @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 332)
-      @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 421)
-      @kong = @brian.items.create(name: "Kong", description: "Great", price: 11, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS2YQS3IP7e4mtm-0y_erFD2slut1LxzZsPPA&usqp=CAU", inventory: 443)
-      @rope = @brian.items.create(name: "Rope", description: "Strong", price: 5, image: "https://bendpetexpress.com/wp-content/uploads/2016/08/dog_toys_mammoth_rope_2knot.jpg ", inventory: 225)
-      @squeeky = @brian.items.create(name: "Squeeky Toy", description: "Pretty Good", price: 12, image: "https://img.chewy.com/is/image/catalog/174770_Main._AC_SL400_V1572616735_.jpg", inventory: 274)
+      @pull_toy = @dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 332)
+      @dog_bone = @dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 421)
+      @kong = @dog_shop.items.create(name: "Kong", description: "Great", price: 11, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS2YQS3IP7e4mtm-0y_erFD2slut1LxzZsPPA&usqp=CAU", inventory: 443)
+      @rope = @dog_shop.items.create(name: "Rope", description: "Strong", price: 5, image: "https://bendpetexpress.com/wp-content/uploads/2016/08/dog_toys_mammoth_rope_2knot.jpg ", inventory: 225)
+      @squeeky = @dog_shop.items.create(name: "Squeeky Toy", description: "Pretty Good", price: 12, image: "https://img.chewy.com/is/image/catalog/174770_Main._AC_SL400_V1572616735_.jpg", inventory: 274)
       @user = User.create!(name: 'Harold Guy',
                           address: '123 Macho St',
                           city: 'Lakewood',
@@ -112,6 +112,27 @@ describe Item, type: :model do
       bottom_three = @order.items.worst_item_stats(3)
 
       expect(bottom_three).to eq([@squeeky, @rope, @pedal])
+    end
+  end
+
+  describe 'discount methods' do
+    before(:each) do
+      @bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 112)
+      @handlebars = @bike_shop.items.create(name: "Handle Bars", description: "Grip it", price: 50, image: "https://cdn.shopify.com/s/files/1/2191/9809/products/bullhorn2__95733.1479507031.1280.960_1024x1024.jpg?v=1501527872", inventory: 243)
+
+      @cart = Cart.new({
+        @handlebars => 74
+        })
+    end
+
+    it '#applicable_discount' do
+      discount = @bike_shop.discounts.create!(quantity: 75, percentage: 20)
+
+      expect(@cart.contents.first[0].applicable_discount(@cart.contents.first[1])).to eq(nil)
+
+      @cart[@handlebars] += 1
+      expect(@cart.contents.first[0].applicable_discount(@cart.contents.first[1])).to eq(discount)
     end
   end
 end
