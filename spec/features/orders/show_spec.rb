017d26080c6384874a105315e7ed2a6767463a31
Zach Stearns <zach.stearns92@gmail.com>
2020-11-10 13:23:15 -0700

    Add test for seeing discounts appear on show

--- spec/features/orders/show_spec.rb
+++ spec/features/orders/show_spec.rb
@@ -93,98 +93,150 @@ describe 'Order show page' do
         expect(page).to have_content('unfulfilled')
       end
     end
+  end
 
-    describe 'As a merchant user' do
-      before :each do
-        @mike = Merchant.create(name: 'Mikemart', address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
-        @meg = Merchant.create(name: 'Megmart', address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
+  describe 'As a merchant user' do
+    before :each do
+      @mike = Merchant.create(name: 'Mikemart', address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
+      @meg = Merchant.create(name: 'Megmart', address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
 
-        @user = User.create!(name: 'Harold Guy',
-                             address: '123 Macho St',
-                             city: 'Lakewood',
-                             state: 'CO',
-                             zip: '80328',
-                             email: 'jystupidd@email.com',
-                             password: 'luggagecombo')
+      @user = User.create!(name: 'Harold Guy',
+                           address: '123 Macho St',
+                           city: 'Lakewood',
+                           state: 'CO',
+                           zip: '80328',
+                           email: 'jystupidd@email.com',
+                           password: 'luggagecombo')
 
-        @mike_user = User.create!(name: 'Mike Guy',
-                                  address: '123 Macho St',
-                                  city: 'Lakewood',
-                                  state: 'CO',
-                                  zip: '80328',
-                                  email: 'mikelol@email.com',
-                                  password: 'luggagecombo',
-                                  role: 1,
-                                  merchant_id: @mike.id)
+      @mike_user = User.create!(name: 'Mike Guy',
+                                address: '123 Macho St',
+                                city: 'Lakewood',
+                                state: 'CO',
+                                zip: '80328',
+                                email: 'mikelol@email.com',
+                                password: 'luggagecombo',
+                                role: 1,
+                                merchant_id: @mike.id)
 
-        @meg_user = User.create!(name: 'Meg Gal',
-                                 address: '123 Macho St',
-                                 city: 'Lakewood',
-                                 state: 'CO',
-                                 zip: '80328',
-                                 email: 'megwoo@email.com',
-                                 password: 'luggagecombo',
-                                 role: 1,
-                                 merchant_id: @meg.id)
+      @meg_user = User.create!(name: 'Meg Gal',
+                               address: '123 Macho St',
+                               city: 'Lakewood',
+                               state: 'CO',
+                               zip: '80328',
+                               email: 'megwoo@email.com',
+                               password: 'luggagecombo',
+                               role: 1,
+                               merchant_id: @meg.id)
 
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@mike_user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@mike_user)
 
-        @tire = @meg.items.create(name: 'Gatorskins', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 12)
-        @paper = @mike.items.create(name: 'Lined Paper', description: 'Great for writing on!', price: 20, image: 'https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png', inventory: 3)
-        @pencil = @mike.items.create(name: 'Pencil', description: 'You can write on paper with it!', price: 2, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
-        @pen = @mike.items.create(name: 'Yellow Pen', description: 'You can write on paper with it!', price: 2, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
-        @inkwell = @mike.items.create(name: 'Yellow Inkwell', description: 'You can write on paper with it!', price: 4, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
-        @lead = @meg.items.create(name: 'Yellow Lead', description: 'You can write on paper with it!', price: 1, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
-        @sticky_note = @mike.items.create(name: 'Yellow Sticky Note', description: 'You can write on paper with it!', price: 4, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
-        @paper_clip = @mike.items.create(name: 'Yellow Paper Clip', description: 'You can write on paper with it!', price: 6, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
-        @push_pin = @meg.items.create(name: 'Yellow Push Pin', description: 'You can write on paper with it!', price: 6, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
-        @stapler = @mike.items.create(name: 'Yellow Stapler', description: 'You can write on paper with it!', price: 10, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
-        @staples = @mike.items.create(name: 'Yellow Staples', description: 'You can write on paper with it!', price: 5, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
+      @tire = @meg.items.create(name: 'Gatorskins', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 12)
+      @paper = @mike.items.create(name: 'Lined Paper', description: 'Great for writing on!', price: 20, image: 'https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png', inventory: 3)
+      @pencil = @mike.items.create(name: 'Pencil', description: 'You can write on paper with it!', price: 2, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
+      @pen = @mike.items.create(name: 'Yellow Pen', description: 'You can write on paper with it!', price: 2, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
+      @inkwell = @mike.items.create(name: 'Yellow Inkwell', description: 'You can write on paper with it!', price: 4, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
+      @lead = @meg.items.create(name: 'Yellow Lead', description: 'You can write on paper with it!', price: 1, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
+      @sticky_note = @mike.items.create(name: 'Yellow Sticky Note', description: 'You can write on paper with it!', price: 4, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
+      @paper_clip = @mike.items.create(name: 'Yellow Paper Clip', description: 'You can write on paper with it!', price: 6, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
+      @push_pin = @meg.items.create(name: 'Yellow Push Pin', description: 'You can write on paper with it!', price: 6, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
+      @stapler = @mike.items.create(name: 'Yellow Stapler', description: 'You can write on paper with it!', price: 10, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
+      @staples = @mike.items.create(name: 'Yellow Staples', description: 'You can write on paper with it!', price: 5, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
 
-        @order2 = @user.orders.create!(name: 'order', address: '123 Main St', city: 'Here', state: 'CO', zip: '58421', user_id: @user.id)
-        @order2.add_item(@tire)
-        @order2.add_item @paper, 7
+      @order2 = @user.orders.create!(name: 'order', address: '123 Main St', city: 'Here', state: 'CO', zip: '58421', user_id: @user.id)
+      @order2.add_item(@tire)
+      @order2.add_item @paper, 7
 
-        @order2 = @user.orders.create!(name: 'second order', address: '754 Main St', city: 'There', state: 'WY', zip: '12421', user_id: @user.id)
-        @order2.add_item @paper, 4
-        @order2.add_item @pencil
+      @order2 = @user.orders.create!(name: 'second order', address: '754 Main St', city: 'There', state: 'WY', zip: '12421', user_id: @user.id)
+      @order2.add_item @paper, 4
+      @order2.add_item @pencil
 
-        @order3 = @user.orders.create!(name: 'third order', address: '754 Main St', city: 'There', state: 'WY', zip: '12421', user_id: @user.id)
-        @order3.add_item @inkwell
-        @order3.add_item @pen, 2
-        @order3.add_item @paper, 6
+      @order3 = @user.orders.create!(name: 'third order', address: '754 Main St', city: 'There', state: 'WY', zip: '12421', user_id: @user.id)
+      @order3.add_item @inkwell
+      @order3.add_item @pen, 2
+      @order3.add_item @paper, 6
 
-        @order4 = @user.orders.create!(name: 'fourth order', address: '754 Main St', city: 'There', state: 'WY', zip: '12421', user_id: @user.id)
-        @order4.add_item @stapler, 2
-        @order4.add_item @staples, 10
-        @order4.add_item @push_pin, 3
+      @order4 = @user.orders.create!(name: 'fourth order', address: '754 Main St', city: 'There', state: 'WY', zip: '12421', user_id: @user.id)
+      @order4.add_item @stapler, 2
+      @order4.add_item @staples, 10
+      @order4.add_item @push_pin, 3
+    end
+
+    it 'Visit order show page' do
+      visit merchant_path
+
+      click_link("Order #{@order4.id}")
+
+      expect(page).to have_content('fourth order')
+      expect(page).to have_content('754 Main St')
+      expect(page).to have_content('12421')
+      expect(page).to have_content('There')
+      expect(page).to have_content('WY')
+
+      expect(page).to have_content(@stapler.name)
+      expect(page).to have_content(@staples.name)
+      expect(page).to_not have_content(@push_pin.name)
+
+      expect(page).to have_link(@stapler.name)
+      expect(page).to have_css("img[src*='#{@stapler.image}']")
+      expect(page).to have_content(@stapler.price)
+      expect(page).to have_content('2')
+
+      expect(page).to have_link(@staples.name)
+      expect(page).to have_css("img[src*='#{@staples.image}']")
+      expect(page).to have_content(@staples.price)
+      expect(page).to have_content('10')
+    end
+  end
+
+  describe 'discounts appear on show' do
+    before :each do
+      @user = User.create!(name: 'Harold Guy',
+                           address: '123 Macho St',
+                           city: 'Lakewood',
+                           state: 'CO',
+                           zip: '80328',
+                           email: 'jyddedharrold@email.com',
+                           password: 'luggagecombo')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80_203)
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
+
+      @twenty_for_ten = @mike.discounts.create(quantity: 20, percentage: 10)
+      @fifty_for_twenty_five = @mike.discounts.create(quantity: 50, percentage: 25)
+
+      @tire = @meg.items.create(name: 'Gatorskins', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 75)
+      @paper = @mike.items.create(name: 'Lined Paper', description: 'Great for writing on!', price: 20, image: 'https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png', inventory: 200)
+      @pencil = @mike.items.create(name: 'Yellow Pencil', description: 'You can write on paper with it!', price: 2, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
+
+      @order_1 = Order.create!(name: 'order', address: '123 Main St', city: 'Here', state: 'CO', zip: '58421', user_id: @user.id)
+      @item_order_1 = @order_1.item_orders.create!(item_id: @tire.id, price: @tire.price, quantity: 2)
+      @item_order_2 = @order_1.item_orders.create!(item_id: @paper.id, price: @paper.price, quantity: 30)
+      @item_order_3 = @order_1.item_orders.create!(item_id: @pencil.id, price: @pencil.price, quantity: 50)
+    end
+
+    it "Discounts show up on the order show page" do
+      visit profile_orders_show_path(@order_1.id)
+
+      within "#item-#{@item_order_1.item_id}" do
+        expect(page).to_not have_content("-$")
       end
 
-      it 'Visit order show page' do
-        visit merchant_path
+      within "#item-#{@item_order_2.item_id}" do
+        expect(page).to have_content("-$60.00")
+      end
 
-        click_link("Order #{@order4.id}")
+      within "#item-#{@item_order_3.item_id}" do
+        expect(page).to have_content("-$25.00")
+      end
 
-        expect(page).to have_content('fourth order')
-        expect(page).to have_content('754 Main St')
-        expect(page).to have_content('12421')
-        expect(page).to have_content('There')
-        expect(page).to have_content('WY')
-
-        expect(page).to have_content(@stapler.name)
-        expect(page).to have_content(@staples.name)
-        expect(page).to_not have_content(@push_pin.name)
-
-        expect(page).to have_link(@stapler.name)
-        expect(page).to have_css("img[src*='#{@stapler.image}']")
-        expect(page).to have_content(@stapler.price)
-        expect(page).to have_content('2')
-
-        expect(page).to have_link(@staples.name)
-        expect(page).to have_css("img[src*='#{@staples.image}']")
-        expect(page).to have_content(@staples.price)
-        expect(page).to have_content('10')
+      within "#grandtotal" do
+        expect(page).to have_content('Total: $900.00')
+        expect(page).to have_content('You Saved $85.00')
+        expect(page).to have_content('Grand Total: $815.00')
       end
     end
   end
 end
+
