19b0e23a7b866620cebf7f88e9503160a457e4f1
Zach Stearns <zach.stearns92@gmail.com>
2020-11-10 12:24:37 -0700

    Add test for total_discounts method

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -31,6 +31,11 @@ describe Order, type: :model do
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
       @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80_210)
 
+      @handlebars = @meg.items.create(name: "Handle Bars", description: "Grip it", price: 50, image: "https://cdn.shopify.com/s/files/1/2191/9809/products/bullhorn2__95733.1479507031.1280.960_1024x1024.jpg?v=1501527872", inventory: 243)
+      @chain = @meg.items.create(name: "Chain", description: "Strong", price: 25, image: "https://dks.scene7.com/is/image/GolfGalaxy/19SCWUBMX12X18CHRTAM?qlt=70&wid=600&fmt=pjpeg", inventory: 232)
+      @pedal = @meg.items.create(name: "Pedal", description: "Step on it", price: 10, image: "https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/11/2020/06/meow.jpg", inventory: 374)
+      @reflector = @meg.items.create(name: "Reflector", description: "They'll see you", price: 5, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcStDVPL-iMrlZHu5mai-EXeyDH-wH2r9nBeFw&usqp=CAU", inventory: 324)
+      @seat = @meg.items.create(name: "Seat", description: "Soft", price: 123, image: "https://i5.walmartimages.com/asr/597375a9-fb8f-4a47-a4a2-28201888754a_1.3ebe254d3fea4ff52d5578eda9e44f77.jpeg", inventory: 217)
       @tire = @meg.items.create(name: 'Gatorskins', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 12)
       @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
       @pull_toy = @brian.items.create(name: 'Pull Toy', description: 'Great pull toy!', price: 10, image: 'http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg', inventory: 32)
@@ -104,6 +109,20 @@ describe Order, type: :model do
       io_3 = @order_1.add_item(@chain, 2)
       expect(@order_1.item_orders.count).to eq(3)
       expect(io_3.quantity).to eq(2)
+    end
+
+    it '#total_discounts' do
+      ten_for_five = @meg.discounts.create(quantity: 10, percentage: 5)
+      fifteen_for_twenty = @meg.discounts.create(quantity: 15, percentage: 20)
+      io_3 = @order_1.item_orders.create!(item: @pedal, price: @pedal.price, quantity: 11)
+      io_4 = @order_1.item_orders.create!(item: @reflector, price: @reflector.price, quantity: 18)
+
+      expect(@io_1.apply_discount).to eq(nil)
+      expect(@io_2.apply_discount).to eq(nil)
+      expect(io_3.apply_discount).to eq(5.5)
+      expect(io_4.apply_discount).to eq(18.0)
+
+      expect(@order_1.total_discounts).to eq(23.5)
     end
   end
 end
